027b16a150e601f672548e6a31c349ff4a47c4bc
RbYudha <youdha900.yr@gmail.com>
2020-04-06 14:03:22 +0700

    Add player movement
    - move left, right, and jump.
    - turn left and right
    - add player camera

---
+++ Assets/playerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    public Rigidbody2D fox;
+
+private void Update ()
+{
+    if (Input.GetKey(KeyCode.A)) {
+        fox.velocity = new Vector2(-5, fox.velocity.y);
+        transform.localScale = new Vector2 (-1, 1);
+    }
+
+    if (Input.GetKey(KeyCode.D)) {
+        //fox.velocity = new Vector2(5, 0);
+        fox.velocity = new Vector2(5, fox.velocity.y);
+        transform.localScale = new Vector2 (1, 1);
+    }
+
+    if (Input.GetKeyDown(KeyCode.Space)) {
+        fox.velocity = new Vector2(fox.velocity.x,5);
+    }
+
+}
+
+}
