663be7afc6368367282820836272325516e0466b
RbYudha <youdha900.yr@gmail.com>
2020-04-15 08:48:19 +0700

    Added Animation 1
    menambahkan animation idle dan running pada Foxelrod.

--- Assets/playerController.cs
+++ Assets/playerController.cs
@@ -5,24 +5,50 @@ using UnityEngine;
 public class playerController : MonoBehaviour
 {
     public Rigidbody2D fox;
+    public Animator foxAnim;
+    private enum State {idle, running, jumping}; //State {0, 1, 2, ....}
+    private State state = State.idle; //keadaan
 
-private void Update ()
+    // private Rigidbody2D fox;
+    //[SerializeField]
+    // private Animator foxAnim;
+
+// private void start() {
+//     fox = GetComponent<Rigidbody2D>();
+//     foxAnim = GetComponent<Animator>();
+// }    
+
+private void Update()
 {
-    if (Input.GetKey(KeyCode.A)) {
+
+    float hDirection = Input.GetAxis("Horizontal");
+
+    // if (Input.GetKey(KeyCode.A)) {
+    if (hDirection < 0) {
         fox.velocity = new Vector2(-5, fox.velocity.y);
         transform.localScale = new Vector2 (-1, 1);
     }
 
-    if (Input.GetKey(KeyCode.D)) {
+    // else if (Input.GetKey(KeyCode.D)) {
+    else if (hDirection > 0) {
         //fox.velocity = new Vector2(5, 0);
         fox.velocity = new Vector2(5, fox.velocity.y);
         transform.localScale = new Vector2 (1, 1);
+    }
+
+    else {
+        
     }
 
     if (Input.GetKeyDown(KeyCode.Space)) {
         fox.velocity = new Vector2(fox.velocity.x,5);
     }
 
+    velocityState();
 }
+
+    private void velocityState() {
+        
+    }
 
 }
